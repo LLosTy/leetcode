d5cfdd0c0243e3975d4e343b0b5a629faca57100
LLosTy <marcojurgenjuras@gmail.com>
2025-05-01 09:25:54 +0200

    feat(2619-ArrayPrototypeLast.js): removed the need for variable

--- 30DaysOfJS/2619-ArrayPrototypeLast.js
+++ 30DaysOfJS/2619-ArrayPrototypeLast.js
@@ -2,15 +2,14 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function () {
-  len = this.length;
-  if (len === 0) {
+  if (this.length === 0) {
     return -1;
   } else {
-    return this[len - 1];
+    return this[this.length - 1];
   }
 };
 
-const arr = [1, 2, 3];
-// const arr = [];
+// const arr = [1, 2, 3];
+const arr = [];
 let a = arr.last(); // 3
 console.log(a);
